4520984ffc1db3745c600a2fc811642bc6178ac8
ABHINAYPRAKASH <127612494+ABHINAYPRAKASH@users.noreply.github.com>
2024-01-08 19:20:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+      /*   String name=sc.next();
+        System.out.println(name);
+         String name1=sc.nextLine();
+        System.out.println(name1);*/
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+        
+    }
+}
